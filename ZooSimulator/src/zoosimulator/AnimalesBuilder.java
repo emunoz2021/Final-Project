f9d036fe493df4cde34b39629290209d1e3924ce
MarioCSalgado <mario_cso15@hotmail.com>
2023-07-01 00:22:11 -0400

    Agregamos mas funciones que consideramos de utilidad en nuestro patron Builder

--- ZooSimulator/src/zoosimulator/AnimalesBuilder.java
+++ ZooSimulator/src/zoosimulator/AnimalesBuilder.java
@@ -4,4 +4,7 @@ import javax.swing.*;
 
 public interface AnimalesBuilder {
     ImageIcon getImagen();
+    void comer(int cantidadComida);
+    boolean verificarNecesidades();
+    boolean esHabitatAdecuado(String habitat);
 }
