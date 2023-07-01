f6109bbebb1a7af8b772ef2786a09a1b84911265
MarioCSalgado <mario_cso15@hotmail.com>
2023-07-01 00:22:47 -0400

    Implementamos las funciones en Gorila

--- ZooSimulator/src/zoosimulator/Gorila.java
+++ ZooSimulator/src/zoosimulator/Gorila.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package zoosimulator;
 
-/**
- *
- * @author mario
- */
-public class Gorila {
-    
+import javax.swing.*;
+
+public class Gorila implements AnimalesBuilder {
+    private ImageIcon Gorila;
+    private int alimentacion; 
+    private String habitat;
+
+    public Gorila(){
+        Gorila = new ImageIcon("gorilla.png");
+        alimentacion = 100;
+        habitat = "Jungla"; 
+    }
+
+    @Override
+    public ImageIcon getImagen() {
+        return Gorila;
+    }
+
+    public void comer(int cantidadComida) {
+        alimentacion += cantidadComida;
+    }
+
+    public boolean verificarNecesidades() {
+        return alimentacion > 0;
+    }
+
+    public boolean esHabitatAdecuado(String habitat) {
+        return this.habitat.equals(habitat);
+    }
 }
