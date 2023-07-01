9c84ee0fa72f19a7085671bdda609670deb1d7e2
MarioCSalgado <mario_cso15@hotmail.com>
2023-06-30 22:38:48 -0400

    Se agrega la subClase BotonJirafa que agrega un JLabel de una jirafa a la Aplicacion

---
+++ ZooSimulator/src/zoosimulator/BotonJirafa.java
@@ -0,0 +1,25 @@
+package zoosimulator;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+
+public class BotonJirafa extends BotonAnimal {
+    private int numJirafas = 0;
+    
+    public BotonJirafa(JPanel panelPrincipal) {
+        super(panelPrincipal, "Jirafa", "jirafaPerfil.jpg");
+    }
+
+    @Override
+    protected void cargarAnimal() {
+        ImageIcon image=new ImageIcon("jirafa.jpg");
+        for(int i=-1; i < numJirafas;i++) {
+            if(i==4) break;
+            JLabel Jirafa = new JLabel(image);
+            add(Jirafa);
+        }
+        numJirafas++;
+        repaint();
+    }
+}
