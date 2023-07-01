6c98d3feca18e7404947bcc138af2acf8a7d57a2
emunoz2021 <emunoz2021@udec.cl>
2023-06-30 23:33:44 -0400

    se usa para separar los oyentes animales de los botones

---
+++ ZooSimulator/src/zoosimulator/PanelExtraAnimales.java
@@ -0,0 +1,139 @@
+package zoosimulator;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class PanelExtraAnimales extends JPanel {
+    BotonGorilla botonGorilla;
+    BotonJirafa botonJirafa;
+    BotonLeon botonLeon;
+    BotonCocodrilo botonCocodrilo;
+    
+    private JFrame ventanita;
+    private PanelsubPrincipal panelSubPrincipal;
+
+    public PanelExtraAnimales(PanelsubPrincipal panelSubPrincipal) {
+        this.panelSubPrincipal = panelSubPrincipal;
+        this.ventanita = panelSubPrincipal.getVentanita();
+        setLayout(new GridLayout(2, 2));
+        botonGorilla = new BotonGorilla(panelSubPrincipal);
+        botonJirafa = new BotonJirafa(panelSubPrincipal);
+        botonLeon = new BotonLeon(panelSubPrincipal);
+        botonCocodrilo = new BotonCocodrilo(panelSubPrincipal);
+        add(botonGorilla);
+        add(botonJirafa);
+        add(botonLeon);
+        add(botonCocodrilo);
+
+        oyenteDeCargaExtra();
+    }
+
+    private void oyenteDeCargaExtra(){
+        oyenteBotonGorilla();
+        oyenteBotonJirafa();
+        oyenteBotonLeon();
+        oyenteBotonCocodrilo();
+    }
+
+    private void oyenteBotonGorilla(){
+        MouseListener listenerGorilla = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                botonGorilla.doClick();
+                ventanita.getLayeredPane().remove(PanelExtraAnimales.this);
+                ventanita.revalidate();
+                ventanita.repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        };
+        
+        botonGorilla.addMouseListener(listenerGorilla);
+    }
+
+    private void oyenteBotonJirafa(){
+        MouseListener listenerJirafa= new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                botonJirafa.doClick();
+                ventanita.getLayeredPane().remove(PanelExtraAnimales.this);
+                ventanita.revalidate();
+                ventanita.repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        };
+        botonJirafa.addMouseListener(listenerJirafa);
+    }
+
+    private void oyenteBotonLeon(){
+        MouseListener listenerLeon = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                botonLeon.doClick();
+                ventanita.getLayeredPane().remove(PanelExtraAnimales.this);
+                ventanita.revalidate();
+                ventanita.repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        };
+        botonLeon.addMouseListener(listenerLeon);
+    }
+
+    private void oyenteBotonCocodrilo(){
+        MouseListener listenerCocodrilo = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                botonCocodrilo.doClick();
+                ventanita.getLayeredPane().remove(PanelExtraAnimales.this);
+                ventanita.revalidate();
+                ventanita.repaint();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        };
+        botonCocodrilo.addMouseListener(listenerCocodrilo);
+    }
+}
