2fb0f9eeff3836f231d05499c8b67049b4f95e43
emunoz2021 <emunoz2021@udec.cl>
2023-06-30 23:29:56 -0400

    se extendio el tamaño de la ventana

--- ZooSimulator/src/zoosimulator/Ventana.java
+++ ZooSimulator/src/zoosimulator/Ventana.java
@@ -9,7 +9,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-
+/**
+ *metodo main para llamar a la ventana y ocurran las acciones subsiguientes
+ */
 public class Ventana extends JFrame{
     PanelPrincipal panel;
     
@@ -19,8 +21,8 @@ public class Ventana extends JFrame{
         panel = new PanelPrincipal(this);
         this.setTitle("Zoo-Simulator");
         this.setResizable(false);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.add(panel,BorderLayout.CENTER); 
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); //cerrar aplicación
+        this.add(panel,BorderLayout.CENTER); //se agrega al centro
         setLocationRelativeTo(null);
         this.setVisible(true);
     }
