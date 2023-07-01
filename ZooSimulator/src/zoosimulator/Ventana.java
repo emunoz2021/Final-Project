da6e0a4b3064c7b70e2d930b6f51ec20314bdd43
emunoz2021 <emunoz2021@udec.cl>
2023-06-30 21:06:24 -0400

    se elimina comentario innecesario

--- ZooSimulator/src/zoosimulator/Ventana.java
+++ ZooSimulator/src/zoosimulator/Ventana.java
@@ -9,9 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-/**
- *metodo main para llamar a la ventana y ocurran las acciones subsiguientes
- */
+
 public class Ventana extends JFrame{
     PanelPrincipal panel;
     
@@ -21,8 +19,8 @@ public class Ventana extends JFrame{
         panel = new PanelPrincipal(this);
         this.setTitle("Zoo-Simulator");
         this.setResizable(false);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE); //cerrar aplicación
-        this.add(panel,BorderLayout.CENTER); //se agrega al centro
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(panel,BorderLayout.CENTER); 
         setLocationRelativeTo(null);
         this.setVisible(true);
     }
